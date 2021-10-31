1b810e9d60c02fed138edb114e552b99a08ad38a
Federico <federicoarredondoc@gmail.com>
2021-10-31 16:37:51 -0300

    fix(sliderjack): fixed second spawning jack not appearing in new saves.

--- Saving/DefaultJson.cs
+++ Saving/DefaultJson.cs
@@ -9,7 +9,7 @@
             LiftsObject objects = new LiftsObject();
 
             objects.lifts.Add(new Lift(0.0f, 147.791458f, 62.132618f, 519.039246f, -0.7204912f, -0.69346416f, true, 0f, 0f, false, 0f, 0f));
-            objects.lifts.Add(new Lift(0.0f, 152.973953f, 62.132618f, 518.845337f, 0.714504f, 0.6996314f, false, 0f, 0f, false, 0f, 100f));
+            objects.lifts.Add(new Lift(0.0f, 152.973953f, 62.132618f, 518.845337f, 0.714504f, 0.6996314f, false, 0f, 0f, false, 0f, 1f));
 
             return objects;
         }
