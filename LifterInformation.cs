ffebbb8e13c2f1d2be5428289f697c9eaa08f321
Federico <federicoarredondoc@gmail.com>
2021-10-31 23:07:40 -0300

    fix(sliderjack): slider jack default height was too high for chad.

--- LifterInformation.cs
+++ LifterInformation.cs
@@ -15,7 +15,7 @@ namespace LifterMod
         private const float minX_jack2Pos = -5.95f;
         private const float maxX_jack2Pos = -3.31f;
 
-        private const float minY_jackHeight = 0.097f;
+        private const float minY_jackHeight = 0.05f;
         private const float maxY_jackHeight = 0.51f;
 
         public float percentage;
